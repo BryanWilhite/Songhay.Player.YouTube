8f4b682396b094f6018c2f5d30f6ab7ce5305767
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-12-31 21:00:11 -0800

    add `StudioFloorPage.YtThumbsSetPage` with `[<EndPoint "/yt-thumbs-set/{key}">]` 🔨

--- Songhay.StudioFloor.Client/Models/StudioFloorPrimitives.fs
+++ Songhay.StudioFloor.Client/Models/StudioFloorPrimitives.fs
@@ -8,6 +8,7 @@ type StudioFloorPage =
     | [<EndPoint "/">] ReadMePage
     | [<EndPoint "/yt-presentation/{key}">] YtPresentationPage of key: string
     | [<EndPoint "/yt-thumbs/{key}">] YtThumbsPage of key: string
+    | [<EndPoint "/yt-thumbs-set/{key}">] YtThumbsSetPage of key: string
 
 type StudioFloorMessage =
     | Error of exn
