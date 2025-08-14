643013b273a30e04bcdb832cf59788b2f23545f3
Bryan Wilhite <bryan.wilhite@gmail.com>
2025-08-13 21:53:56 -0700

    remove `YtUriUtility.getPresentationUri` 🔨🔥

--- Songhay.Player.YouTube/YtUriUtility.fs
+++ Songhay.Player.YouTube/YtUriUtility.fs
@@ -1,10 +1,7 @@
 namespace Songhay.Player.YouTube
 
 open System
-open Songhay.Modules.Models
 open Songhay.Modules.StringUtility
-
-open Songhay.Player.YouTube.YouTubeScalars
 
 module YtUriUtility =
 
@@ -13,9 +10,6 @@ module YtUriUtility =
 
     [<Literal>]
     let YtIndexSonghayTopTen = "youtube-index-songhay-top-ten"
-
-    let getPresentationUri (id: Identifier) =
-        Uri($"{YouTubeApiRootUri}{YouTubeApiVideosPath}{id.StringValue}", UriKind.Absolute)
 
     let getYtSetKey seed (uri: Uri) =
         let prefix = seed |> toKabobCase |> Option.defaultValue String.Empty
