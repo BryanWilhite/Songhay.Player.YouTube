d984722646924f949b5a0919705e876dbb1594fa
Bryan Wilhite <bryan.wilhite@gmail.com>
2025-08-14 10:27:26 -0700

    remove `YtUriUtility.getYtSetKey` 🔨🔥

--- Songhay.Player.YouTube.Tests/ServiceHandlerUtilityTests.fs
+++ Songhay.Player.YouTube.Tests/ServiceHandlerUtilityTests.fs
@@ -5,7 +5,6 @@ open Microsoft.Extensions.Logging
 open NSubstitute
 
 open Xunit
-open Xunit.Abstractions
 open FsUnit.Xunit
 open FsUnit.CustomMatchers
 
@@ -14,29 +13,8 @@ open Songhay.Modules.Bolero.JsonDocumentUtility
 
 open Songhay.Player.YouTube.Models
 open Songhay.Player.YouTube.ServiceHandlerUtility
-open Songhay.Player.YouTube.YtUriUtility
 
-type ServiceHandlerUtilityTests(testOutputHelper: ITestOutputHelper) =
-
-    [<SkippableTheory>]
-    [<InlineData("songhay/news", "called-yt-set-news")>]
-    member this.``getYtSetKey test`` (pathSegments: string, expected: string) =
-        Skip.If(studioSettingsPath.IsNone, studioSettingsPathMessage)
-
-        let pathSegmentsArray = pathSegments.Split('/')
-        pathSegmentsArray.Length |> should equal 2
-
-        let tuple = ((Identifier.fromString pathSegmentsArray[0]), (ClientId.fromString pathSegmentsArray[1]))
-        let uriOption = tuple ||> model.getPlaylistSetUri
-        uriOption |> should be (ofCase <@ Option.Some @>)
-
-        testOutputHelper.WriteLine $"{nameof uriOption}: {uriOption.Value.OriginalString}"
-
-        let seed = nameof YouTubeMessage.CalledYtSet
-        let cacheKey = uriOption.Value |> getYtSetKey seed
-        cacheKey |> should equal expected
-
-        testOutputHelper.WriteLine $"{nameof cacheKey}: {cacheKey}"
+type ServiceHandlerUtilityTests() =
 
     [<Theory>]
     [<InlineData("youtube-index-songhay-top-ten.json")>]
--- Songhay.Player.YouTube/YtUriUtility.fs
+++ Songhay.Player.YouTube/YtUriUtility.fs
@@ -10,7 +10,3 @@ module YtUriUtility =
 
     [<Literal>]
     let YtIndexSonghayTopTen = "youtube-index-songhay-top-ten"
-
-    let getYtSetKey seed (uri: Uri) =
-        let prefix = seed |> toKabobCase |> Option.defaultValue String.Empty
-        $"{prefix}-{uri.Segments |> Array.last}"
